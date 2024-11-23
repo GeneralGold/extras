f48b17d6169b6e740be653fddfbacb361d383c8c
GeneralGold <102259050+GeneralGold@users.noreply.github.com>
2024-11-23 22:12:00 +0000

    hrmmm

--- aboutme/links.js
+++ aboutme/links.js
@@ -5,6 +5,8 @@ export const links = [
 "https://youtube.com/@GeneralGoldYT",
         image:
 "https://generalgoldyt.com/website-images/youtube.png",
+        image2:
+        "https://generalgoldyt.com/aboutme/pride/trans.png",
     },
     {
         name: "Twitch",
@@ -12,6 +14,8 @@ export const links = [
 "https://twitch.tv/generalgoldyt",
         image: 
 "https://generalgoldyt.com/downloads/4c54fd951eefd4f62e280832d98d6369.jpg",
+        image2:
+        "https://generalgoldyt.com/aboutme/pride/biromantic.png",
     },
     {
         name: "VODs Channel",
@@ -19,5 +23,7 @@ export const links = [
 "https://youtube.com/@GeneralGoldVODS",
         image: 
 "https://generalgoldyt.com/website-images/youtube.png",
+        image2:
+        "https://generalgoldyt.com/aboutme/pride/aegosexual.png",
     },
 ];
--- aboutme/main.js
+++ aboutme/main.js
@@ -2,12 +2,12 @@ import { links } from "./links.js";
 
 const linkContainer = document.getElementById("links");
 
-function addLink(name, link, image) {
+function addLink(name, link, image, image2) {
     return `
   <a href="${link}" class="link" target="blank">
     <img src="${image}"/>
     <span>${name}  </span>
-    <img class="linkIcon" src="link-solid.svg" alt=""/>
+    <img class="linkIcon" src="${image2}" alt=""/>
   </a>
   `;
 }
