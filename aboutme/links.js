d82e4fc9d50b34d7898e01764ccca63757b43003
GeneralGold <102259050+GeneralGold@users.noreply.github.com>
2024-11-23 21:49:20 +0000

    okie

--- aboutme/links.js
+++ aboutme/links.js
@@ -4,20 +4,20 @@ export const links = [
         link:
 "https://youtube.com/@GeneralGoldYT",
         image:
-"/downloads/c7ecf1a2ed0abfae2fc3a298b3e47657.jpg",
+"https://generalgoldyt.com/downloads/c7ecf1a2ed0abfae2fc3a298b3e47657.jpg",
     },
     {
         name: "Twitch",
         link: 
 "https://twitch.tv/generalgoldyt",
         image: 
-"/downloads/4c54fd951eefd4f62e280832d98d6369.jpg",
+"https://generalgoldyt.com/downloads/4c54fd951eefd4f62e280832d98d6369.jpg",
     },
     {
         name: "VODs Channel",
         link: 
 "https://youtube.com/@GeneralGoldVODS",
         image: 
-"/downloads/c7ecf1a2ed0abfae2fc3a298b3e47657.jpg",
+"https://generalgoldyt.com/downloads/c7ecf1a2ed0abfae2fc3a298b3e47657.jpg",
     },
 ];
