51677fd487f4858f8709458b4a9e301854d438a9
GeneralGold <102259050+GeneralGold@users.noreply.github.com>
2024-11-23 21:46:36 +0000

    w

--- aboutme/links.js
+++ aboutme/links.js
@@ -4,20 +4,20 @@ export const links = [
         link:
 "https://youtube.com/@GeneralGoldYT",
         image:
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+"/downloads/c7ecf1a2ed0abfae2fc3a298b3e47657.jpg",
     },
     {
         name: "Twitch",
         link: 
 "https://twitch.tv/generalgoldyt",
         image: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+"/downloads/4c54fd951eefd4f62e280832d98d6369.jpg",
     },
     {
         name: "VODs Channel",
         link: 
 "https://youtube.com/@GeneralGoldVODS",
         image: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+"/downloads/c7ecf1a2ed0abfae2fc3a298b3e47657.jpg",
     },
 ];
