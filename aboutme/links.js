3b788fff1be672097c428568285d86d534bac1e4
GeneralGold <102259050+GeneralGold@users.noreply.github.com>
2024-11-23 21:42:01 +0000

    #killme

---
+++ aboutme/links.js
@@ -0,0 +1,23 @@
+export const links = [
+        {
+        name: "YouTube",
+        link:
+"https://youtube.com/@GeneralGoldYT",
+        image:
+"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+    },
+    {
+        name: "Twitch",
+        link: 
+"https://twitch.tv/generalgoldyt",
+        image: 
+"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+    },
+    {
+        name: "VODs Channel",
+        link: 
+"https://youtube.com/@GeneralGoldVODS",
+        image: 
+"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+    },
+];
